77a732efa421e19b19f3f0d0a8dda23c800eb36e
Whitedragon1987 <vanoostenjohan@hotmail.com>
2021-12-28 11:32:55 +0100

    upload and download incl download zip database

---
+++ src/main/java/nl/novi/uploaddownload/service/DatabaseService.java
@@ -0,0 +1,42 @@
+package nl.novi.uploaddownload.service;
+
+import nl.novi.uploaddownload.repository.DocFileDao;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+@Service
+public class DatabaseService {
+    private DocFileDao doc;
+
+    public DatabaseService(DocFileDao doc){
+        this.doc = doc;
+    }
+
+    public Resource downLoadFileDatabase(String fileName) {
+
+
+        String url = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFromDB/").path(fileName).toUriString();
+
+        Resource resource;
+
+        try {
+            resource = new UrlResource(url);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Issue in reading the file", e);
+        }
+
+        if(resource.exists()&& resource.isReadable()) {
+            return resource;
+        } else {
+            throw new RuntimeException("the file doesn't exist or not readable");
+        }
+    }
+}
